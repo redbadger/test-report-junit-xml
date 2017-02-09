5ed755113b1b80546469d7a398dac7091d142210
Andrew Haines <andrew.haines@red-badger.com>
2017-02-09 12:26:56 +0000

    Add description

--- lein-test-report-junit-xml/project.clj
+++ lein-test-report-junit-xml/project.clj
@@ -1,6 +1,7 @@
 (defproject lein-test-report-junit-xml "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Lein plugin providing JUnit XML output for clojure.test"
   :url "https://github.com/redbadger/test-report-junit-xml"
+  :scm {:dir ".."}
   :license {:name "Eclipse Public License", :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[lein-test-report "0.1.0-SNAPSHOT"]]
   :eval-in-leiningen true)
--- test-report-junit-xml/project.clj
+++ test-report-junit-xml/project.clj
@@ -1,6 +1,7 @@
 (defproject test-report-junit-xml "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Library providing JUnit XML output for clojure.test"
   :url "https://github.com/redbadger/test-report-junit-xml"
+  :scm {:dir ".."}
   :license {:name "Eclipse Public License", :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/data.xml "0.2.0-alpha2"]])
