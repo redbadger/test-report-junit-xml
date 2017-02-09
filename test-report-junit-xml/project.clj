0e63ad71b9336a4cf75aef82714f365214240f58
Andrew Haines <andrew.haines@red-badger.com>
2017-02-09 12:31:14 +0000

    Make test-report an explicit dependency

--- test-report-junit-xml/project.clj
+++ test-report-junit-xml/project.clj
@@ -4,4 +4,5 @@
   :scm {:dir ".."}
   :license {:name "Eclipse Public License", :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/data.xml "0.2.0-alpha2"]])
+                 [org.clojure/data.xml "0.2.0-alpha2"]
+                 [test-report "0.1.0-SNAPSHOT"]])
