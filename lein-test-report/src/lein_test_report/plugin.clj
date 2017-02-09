3da432efcd17d9704cd3ab9f99a2331bb448d902
Andrew Haines <andrew.haines@red-badger.com>
2017-02-09 10:46:12 +0000

    Split repo
    https://github.com/redbadger/test-report
    https://github.com/redbadger/test-report-junit-xml
    https://github.com/redbadger/test-report-junit-xml-example

--- lein-test-report/src/lein_test_report/plugin.clj
+++
@@ -1,8 +0,0 @@
-(ns lein-test-report.plugin
-  (:require [lein-test-report.utils :refer [add-profile]]))
-
-(defn middleware [project]
-  (let [options (:test-report project {})]
-    (add-profile project {:dependencies [['test-report "0.1.0-SNAPSHOT"]]
-                          :injections `[(require 'test-report.core)
-                                        (test-report.core/activate ~options)]})))
--- lein-test-report/src/lein_test_report/utils.clj
+++
@@ -1,7 +0,0 @@
-(ns lein-test-report.utils
-  (:require [leiningen.core.project :as project]))
-
-(defn add-profile [project profile]
-  (if (some #{profile} (-> project meta :included-profiles))
-    project
-    (project/merge-profiles project [profile])))
