d4aa2225962afd81cb78c8b090d3bcb02b43f364
Andrew Haines <andrew.haines@red-badger.com>
2017-02-14 15:54:13 +0000

    Package test-report-junit-xml as a shaded uberjar

--- test-report-junit-xml/project.clj
+++ test-report-junit-xml/project.clj
@@ -1,9 +1,15 @@
-(defproject test-report-junit-xml "0.1.0"
+(defproject test-report-junit-xml "0.2.0-SNAPSHOT"
   :description "Library providing JUnit XML output for clojure.test"
   :url "https://github.com/redbadger/test-report-junit-xml"
   :scm {:dir ".."}
   :license {:name "Eclipse Public License", :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/data.xml "0.2.0-alpha2"]
-                 [test-report "0.1.0"]]
-  :profiles {:test {:resource-paths ["test/resources"]}})
+  :plugins [[lein-shade "0.1.0"]]
+  :profiles {:test {:resource-paths ["test/resources"]}
+             :uberjar {:aot :all}
+             :provided {:dependencies [[org.clojure/clojure "1.8.0"]]}
+             :shaded {:dependencies [[org.clojure/data.xml "0.2.0-alpha2" :exclusions [org.clojure/clojure]]
+                                     [test-report "0.2.0-SNAPSHOT"]]
+                      :shade {:namespaces [clojure.data.xml
+                                           test-report]}}
+             :default [:leiningen/default :shaded]}
+  :aliases {"deploy" ["deploy-shaded-jar" "clojars"]})
