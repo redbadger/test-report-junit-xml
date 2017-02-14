3f449df7bc70a792ba0cdb18f8dbef5581bb0e08
Andrew Haines <andrew.haines@red-badger.com>
2017-02-14 16:00:41 +0000

    Default deploy repository to clojars

--- lein-test-report-junit-xml/project.clj
+++ lein-test-report-junit-xml/project.clj
@@ -4,4 +4,5 @@
   :scm {:dir ".."}
   :license {:name "Eclipse Public License", :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[lein-test-report "0.2.0-SNAPSHOT"]]
+  :aliases {"deploy" ["deploy" "clojars"]}
   :eval-in-leiningen true)
