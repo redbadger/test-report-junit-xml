742fc130483a36a6e3a6635a5ead8748ebee8b4a
Andrew Haines <andrew.haines@red-badger.com>
2017-02-15 11:42:35 +0000

    Update shade plugin

--- test-report-junit-xml/project.clj
+++ test-report-junit-xml/project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/redbadger/test-report-junit-xml"
   :scm {:dir ".."}
   :license {:name "Eclipse Public License", :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :plugins [[lein-shade "0.2.0"]]
+  :plugins [[lein-shade "0.3.0"]]
   :profiles {:test {:resource-paths ["test/resources"]}
              :uberjar {:aot :all}
              :provided {:dependencies [[org.clojure/clojure "1.8.0"]]}
