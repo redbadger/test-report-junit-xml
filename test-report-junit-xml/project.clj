ffffdffbc4ac2f3d4a482a361d3884f1ee21b3c9
Andrew Haines <andrew.haines@red-badger.com>
2017-02-15 13:54:47 +0000

    Don't shade test-report

--- test-report-junit-xml/project.clj
+++ test-report-junit-xml/project.clj
@@ -7,10 +7,9 @@
   :profiles {:test {:resource-paths ["test/resources"]}
              :uberjar {:aot :all}
              :provided {:dependencies [[org.clojure/clojure "1.8.0"]]}
-             :shaded {:dependencies [[org.clojure/data.xml "0.2.0-alpha2" :exclusions [org.clojure/clojure]]
-                                     [test-report "0.2.0-SNAPSHOT"]]
-                      :shade {:namespaces [clojure.data.xml
-                                           test-report]}}
-             :default [:leiningen/default :shaded]}
+             :shaded {:dependencies [[org.clojure/data.xml "0.2.0-alpha2" :exclusions [org.clojure/clojure]]]
+                      :shade {:namespaces [clojure.data.xml]}}
+             :unshaded ^:leaky {:dependencies [[test-report "0.2.0-SNAPSHOT"]]}
+             :default [:leiningen/default :shaded :unshaded]}
   :aliases {"deploy" ["deploy-shaded-jar" "clojars"]
             "install" ["install-shaded-jar"]})
